7cf84ad70b8562a8d505c850de513eca7c45a973
Dahee Lee <ai04120@naver.com>
2025-02-15 22:29:52 +0900

    feat: 직원추가모달 추가중

---
+++ src/components/molecules/Manage/AddModal.tsx
@@ -0,0 +1,203 @@
+import styled from "styled-components";
+import { ReactComponent as CloseIcon } from "assets/Close.svg";
+import { Dispatch, SetStateAction } from "react";
+import {
+  SignAgreeWrap,
+  SignDesc,
+  SignLabel,
+  SignLabelWrap,
+  SignPersonal
+} from "components/atoms/SignUp/SignUpAtoms";
+import TextField from "components/atoms/TextField";
+import { useForm } from "react-hook-form";
+import Dropdown from "components/atoms/Dropdown";
+import Button from "components/atoms/Button";
+
+interface Props {
+  setIsModalActive: Dispatch<SetStateAction<boolean>>;
+}
+
+const AddModal = ({ setIsModalActive }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setError,
+    clearErrors,
+    setValue,
+    control,
+    formState: { errors }
+  } = useForm();
+  const handlePostcode = () => {
+    console.log(123);
+  };
+
+  return (
+    <ModalWrap>
+      <TitleWrap>
+        <h3>
+          직원 정보 <span>입력</span>
+        </h3>
+        <div onClick={() => setIsModalActive(false)}>
+          <CloseIcon />
+        </div>
+      </TitleWrap>
+      <form action="">
+        <InfoTitle>기본 정보</InfoTitle>
+        <InputWrap>
+          <div className="short">
+            <SignLabel>이름</SignLabel>
+            <TextField placeholder="이름을 입력해주세요" style={{ width: "313px" }} />
+          </div>
+          <div className="short">
+            <SignLabel>생년월일</SignLabel>
+            <TextField placeholder="0000.00.00" style={{ width: "313px" }} />
+          </div>
+        </InputWrap>
+        <InputWrap>
+          <div className="short">
+            <SignLabel>연락처</SignLabel>
+            <TextField placeholder="010-0000-000" style={{ width: "313px" }} />
+          </div>
+        </InputWrap>
+        <InputWrap>
+          <div>
+            <SignLabel>사업장 소재지</SignLabel>
+            <TextField
+              style={{ marginBottom: "16px" }}
+              placeholder="클릭해서 도로명, 지번 주소를 검색해주세요"
+              onFocus={handlePostcode}
+              {...register("address1")}
+            />
+            <TextField placeholder="상세 주소지를 입력해주세요" {...register("address2")} />
+          </div>
+        </InputWrap>
+        <InputWrap>
+          <div className="short">
+            <SignLabel>근무 시작일</SignLabel>
+            <TextField placeholder="0000.00.00" style={{ width: "313px" }} />
+          </div>
+          <div className="short">
+            <SignLabel>근무 종료일</SignLabel>
+            <TextField placeholder="0000.00.00" style={{ width: "313px" }} />
+          </div>
+        </InputWrap>
+        <InputWrap style={{ marginBottom: "120px" }}>
+          <div>
+            <SignLabel>급여 계좌</SignLabel>
+            <TextField placeholder="계좌" />
+          </div>
+        </InputWrap>
+        <InfoTitle>근무 정보</InfoTitle>
+        <InputWrap>
+          <div>
+            <SignLabel>근무일</SignLabel>
+            <Dropdown name="day" control={control} placeholder="요일 선택" />
+          </div>
+        </InputWrap>
+        <InputWrap>
+          <div>
+            <SignLabel>근무 시간</SignLabel>
+            <TextField placeholder="시간" />
+          </div>
+        </InputWrap>
+        <InputWrap>
+          <div>
+            <SignLabel>가능한 추가 근무일</SignLabel>
+            <Dropdown name="day1" control={control} placeholder="요일 선택" />
+          </div>
+        </InputWrap>
+        <InputWrap style={{ marginBottom: "60px" }}>
+          <div>
+            <SignLabel>근무 시간</SignLabel>
+            <TextField placeholder="시간" />
+          </div>
+        </InputWrap>
+        <InputWrap style={{ marginBottom: "120px" }}>
+          <div>
+            <SignLabel>특이사항</SignLabel>
+            <TextField placeholder="특이사항" />
+          </div>
+        </InputWrap>
+        <Button text="저장하기" />
+      </form>
+    </ModalWrap>
+  );
+};
+
+export default AddModal;
+
+const ModalWrap = styled.div`
+  width: 827px;
+  height: 620px;
+  border-radius: 24px;
+  background: #fafafa;
+  box-shadow: 0px 2px 12px 0px rgba(20, 20, 43, 0.08);
+  margin-top: 40px;
+  padding: 54px 72px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    width: 8px;
+    padding-right: 5px;
+    margin: 50px 0;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    width: 3px;
+    height: 36px;
+    background: #c5c5c5;
+    border-radius: 10px;
+    border-right: 8px solid white;
+    border-left: 6px solid #c5c5c5;
+  }
+`;
+
+const TitleWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  margin-bottom: 32px;
+
+  * {
+    font-weight: 700;
+    font-size: 36px;
+  }
+
+  span {
+    color: var(--main-1);
+  }
+
+  div {
+    padding-left: 10px;
+    cursor: pointer;
+
+    svg {
+      width: 24px;
+      height: 24px;
+    }
+  }
+`;
+
+const InfoTitle = styled.div`
+  font-weight: 600;
+  font-size: 28px;
+  color: #363636;
+  margin-bottom: 40px;
+`;
+
+const InputWrap = styled.div`
+  display: flex;
+  gap: 42px;
+  margin-bottom: 32px;
+
+  .short {
+    width: 313px;
+  }
+`;
